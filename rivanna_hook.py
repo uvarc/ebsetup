60fc2c2c2c2f74609980faa62dffda9208ead9ca
Ruoshi_Sun <ruoshi@gmail.com>
2023-04-18 14:51:29 -0400

    application class

--- rivanna_hook.py
+++ rivanna_hook.py
@@ -1,3 +1,11 @@
 def pre_module_hook(self):
+    """add *_ROOT environment variable by default"""
     from easybuild.tools.filetools import convert_name
     self.cfg.update('modextravars', {'%s_ROOT' % convert_name(self.name, upper=True): self.installdir})
+
+def module_write_hook(self, filepath, module_txt, *args, **kwargs):
+    """URL is already taken care of by homepage. Preserve "Application class" in 3.9 for website shortcode."""
+    import re
+    search = r'whatis\(\[==\[URL:.*'
+    replace = 'whatis([==[Application class: %s]==])' % self.cfg['moduleclass']
+    return re.sub(search, replace, module_txt)
