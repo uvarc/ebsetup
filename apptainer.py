cad88bbbd94bd58a3af44ea71cf72c231bf10b3f
Ruoshi_Sun <ruoshi@gmail.com>
2025-03-25 13:22:18 -0400

    Singularity->Apptainer

--- apptainer.py
+++ apptainer.py
@@ -1,5 +1,5 @@
 from easybuild.tools.toolchain.container import Container
 
-class Singularity(Container):
+class Apptainer(Container):
     """Container toolchain with apptainer."""
     NAME = 'apptainer'
