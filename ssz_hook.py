8eff5e7cc8767f4d60d303d85d26d2705e374693
Ruoshi_Sun <ruoshi@gmail.com>
2026-08-21 14:42:42 -0400

    exclude pyenv from *_ROOT hook

--- ssz_hook.py
+++ ssz_hook.py
@@ -18,11 +18,15 @@ def pre_module_hook(self):
     the first character of an env var cannot be a digit
     """
     from easybuild.tools.filetools import convert_name
-    if self.name[0].isdigit():
-        name = '_' + self.name
-    else:
-        name = self.name
-    self.cfg.update('modextravars', {'%s_ROOT' % convert_name(name, upper=True): self.installdir})
+    exclude = [
+        'pyenv', # need to set PYENV_ROOT=$HOME/.pyenv
+    ]
+    if self.name not in exclude:
+        if self.name[0].isdigit():
+            name = '_' + self.name
+        else:
+            name = self.name
+        self.cfg.update('modextravars', {'%s_ROOT' % convert_name(name, upper=True): self.installdir})
 
 def module_write_hook(self, filepath, module_txt, *args, **kwargs):
     """URL is already taken care of by homepage. Preserve "Application class" in 3.9 for website shortcode."""
